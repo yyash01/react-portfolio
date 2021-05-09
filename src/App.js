140509e32f0b1c83c401e458d09bcfd2cfa602d9
yyash01 <yjain3.436@gmail.com>
2021-05-09 16:19:05 +0530

    added react starting structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>this  op is yash</h1>
+                <h2>here to rock</h2>
+            </div>
+        )
+    }
+}
+
+export default App;
