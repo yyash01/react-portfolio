5223ef147e2794f045c6bdb0baca619fd93e13d5
yyash01 <yjain3.436@gmail.com>
2021-05-09 18:26:58 +0530

    Learning DAY-1

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+
+    return(
+    <div className="note">
+        <h1>This is the title</h1>
+        <p>Write your note here</p>
+    </div>
+
+    );
+    
+}
+
+export default Note;
+
