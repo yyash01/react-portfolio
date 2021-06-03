ae67bd90aa03ced4f53a2c9e651058f8e539c594
yyash01 <yjain3.436@gmail.com>
2021-06-03 19:43:25 +0530

    1.3 added changes in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,12 +6,18 @@ import CreateArea from "./CreateArea";
 
 function App() {
 
-    
+    //1.3 - The next sub step is to add this new note to an array.
+
+    //that array is going to need state because it's going to be changed.
+    //so for this lets create a constant (notes) and (setNotes) - for updating the notes array.
+    const [notes , setNotes] = useState([]);
 
 
     //create a function called addNote and this addNote is going to receive a note
-    function addNote(note) {
-        console.log(note);
+    function addNote(newNote) {
+        setNotes(prevNotes =>{
+        return [...prevNotes , newNote];
+        })
         
     }
 
